1d99bb1927fe04ed23f842e65c9d7a3f0aedb119
aymensellaouti <aymen.sellaouti@gmail.com>
2023-01-10 14:47:18 +0100

    update API link

--- src/app/cv/services/cv.service.ts
+++ src/app/cv/services/cv.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Personne} from '../model/personne';
 import {Observable, Subject} from 'rxjs';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-const API_LINK = 'https://immense-citadel-91115.herokuapp.com/api/personnes/';
+const API_LINK = 'https://apilb.tridevs.net/explorer/api/personnes/';
 @Injectable({
   providedIn: 'root'
 })
